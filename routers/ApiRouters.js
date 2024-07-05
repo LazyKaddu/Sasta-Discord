b02f91f462d6823e03fc5c4a1326ccb32457a738
Nikhil <nikhilbhatt220705@gmail.com>
2024-07-05 16:25:13 +0530

    Models Updated!

--- routers/ApiRouters.js
+++ routers/ApiRouters.js
@@ -1,8 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const userModel = require("../models/user-model");
 
-router.get("/", (req,res)=>{
-    res.send("hey");
+router.get("/get-user", async (req, res) => {
+  try {
+    const user = await userModel.findById(req.session.passport.user);
+    if (!user) {
+      return res.status(404).json({ error: "No User Found!" });
+    }
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/get-user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await userModel.findById(id);
+    if (user) {
+      return res.status(200).json(user);
+    }
+    return res.status(404).json({ error: "No User Found!" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
