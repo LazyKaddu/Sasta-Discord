6e1f2fc9f453c7726a25615103d93d13a2daca27
Nikhil <nikhilbhatt220705@gmail.com>
2024-07-08 01:22:47 +0530

    Updated Login/Register API's

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,18 +3,18 @@ const passport = require('passport');
 exports.login = (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if (err) return next(err);
-    if (!user) return res.status(400).json({ message: info.message });
+    if (!user) return res.json({ message: 'Username or Password is Incorrect' });
 
     req.logIn(user, (err) => {
       if (err) return next(err);
-      return res.status(200).json({ message: 'Logged in successfully', user });
+      return res.json({ message: 'Logged in successfully', user });
     });
   })(req, res, next);
 };
 
 exports.logout = (req, res) => {
   req.logout((err) => {
-    if (err) return res.status(500).json({ message: 'Error logging out' });
-    res.status(200).json({ message: 'Logged out successfully' });
+    if (err) return res.json({ error : 'Error logging out' });
+    res.json({ success : 'Logged out successfully' });
   });
 };
--- routers/User-Routers/usersRouter.js
+++ routers/User-Routers/usersRouter.js
@@ -8,22 +8,27 @@ const authController = require('../../controllers/authController');
 passport.use(new localStrategy(userModel.authenticate()));
 
 
-router.post('/register', function(req, res){
-  const { username, email, password } = req.body
-
-  if (!username || !email || !password) {
-    return res.json({error : 'Arguments missing'});
-  }
-  
+router.post('/register', async function(req, res){
   try {
+    const { username, email, password } = req.body
+
+    if (!username || !email || !password) {
+      return res.json({error : 'Arguments missing'});
+    }
+
+    const ifUser = await userModel.findOne({username : username});
+    if (ifUser) {
+      return res.json({error : 'A user with the given username is already registered'});
+    }
+  
     const userData = new userModel({ username, email })
     userModel.register(userData, password).then(function() {
         passport.authenticate('local')(req, res, function(){
-          res.json({success : 'User created'});
+          res.json({success : 'User created', user_id: userData._id, username });
         })
       })
   } catch (error) {
-    res.json({error : 'Error Occured'});
+    res.json({error : 'Error Occured! Try again'});
   }
 })
 
