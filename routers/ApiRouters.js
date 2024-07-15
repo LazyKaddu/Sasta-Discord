05e8d83638a4504fa4769a2470272ea486d20c55
Aashish Negi <aashishnegi0000@gmail.com>
2024-07-15 11:55:34 +0530

    created contactRouter

--- routers/ApiRouters.js
+++ routers/ApiRouters.js
@@ -5,12 +5,14 @@ const serverRouter = require('./Server-Routers/server-api');
 const channelRouter = require('./Channel-Routers/channel-api');
 const messageRouter = require('./Message-Routers/message-api');
 const userRouter = require('./User-Routers/usersRouter');
+const contactRouter = require('./Channel-Routers/channel-api');
 
 
 router.use('/server', serverRouter);
 router.use('/channel', channelRouter);
 router.use('/message', messageRouter);
 router.use('/user', userRouter);
+router.use('/contacted',contactRouter);
 
 
 module.exports = router;
---
+++ routers/Contact-Routers/contact-api.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const nodeMailer = require('nodemailer');
+
+
+
+
+router.get('/contact', async(req,res)=>{
+    const { name, email, message } = req.body;
+
+    const transporter = nodeMailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user: 'mehengadiscord@gmail.com',
+            pass: ''
+        }
+    });
+    
+    const mailoption = {
+        from:'mehengadiscord@gmail.com',
+        to: 'aashishnegi0000@gmail.com, nikhil220705@gmail.com',
+        subject: 'bhai kaam kr raha hai kya yeh',
+        text: `name - ${name} email - ${email} says - ${message} at - ${Date.now}`
+    };
+
+    transporter.sendMail(mailoption, (error,info)=>{
+        if(error){
+            res.json({ error : 'mail not send'});
+        }
+        else if (info){
+            res.json({ success : info });
+        }
+    })
+
+})
