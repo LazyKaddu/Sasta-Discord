f449dcf76b89fc1ed914b1e15932138b20e2615f
Aashish Negi <aashishnegi0000@gmail.com>
2024-07-18 16:48:13 +0530

    socket fixx

--- utils/socket-setup.js
+++ utils/socket-setup.js
@@ -13,15 +13,22 @@ function initializeSocket(io) {
       socket.userId = userId;
       socket.serverId = serverId;
 
-      try{
+      try {
         const user = await userModel.findById(userId);
-        const server = await channelModel.findById(serverId);
-        const message = await server.message
-        socket.emit("existing message", message);
-      } catch {
-        console.log('error in join group');
+        const server = await serverModel.findById(serverId);
+        try {
+          const messages = await server.message.map((item) => Message.findById(item))
+          console.log(messages);
+          socket.emit("existing message", messages);
+        } catch (e) {
+          console.log("no previous messages " + e);
+          socket.emit("existing message", [])
+        }
+
+        console.log(`${user} joined channel ${server}`);
+      } catch (e) {
+        console.log('error in join group ' + e);
       }
-      console.log(`${userId} joined channel ${serverId}`);
 
 
     });
@@ -33,11 +40,13 @@ function initializeSocket(io) {
 
     socket.on("chat message", async ({ userId, userName, serverId, message }) => {
       const server = await serverModel.findById(serverId);
+      console.log(serverId)
       const msg = await Message.create({
         content: message,
         sender: userName,
         server: serverId,
       });
+      await msg.save();
       server.message.push(msg);
       await server.save()
       io.to(serverId).emit("chat message", msg);
